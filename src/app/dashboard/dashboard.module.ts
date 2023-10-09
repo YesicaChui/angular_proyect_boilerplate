431cadca57c3cb797357fe18ef732d49afe8c604
YesicaChui <yesicachuic@gmail.com>
2023-10-09 14:22:03 -0500

    agregando sidenav

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 
-
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
     DashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSidenavModule
+  ],
+  exports :[
+    DashboardComponent
   ]
+
 })
 export class DashboardModule { }
