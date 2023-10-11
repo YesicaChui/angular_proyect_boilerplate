8142fae0d4bf3b8d32fbc529cdcf54a719a2a6fc
YesicaChui <yesicachuic@gmail.com>
2023-10-11 00:58:23 -0500

    agregando validaciones al formulario generando componente users

--- src/app/dashboard/pages/forns/forns.component.ts
+++ src/app/dashboard/pages/forns/forns.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forns',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./forns.component.scss']
 })
 export class FornsComponent {
+  userForm: FormGroup
 
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3),Validators.maxLength(10)] ],
+      lastName: ['', Validators.required]
+    })
+  }
+  get nameControl() {
+    return this.userForm.get('name')
+  }
 }
+
+
